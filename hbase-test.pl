c7d6538da4a29f8310aa0fe803a69bbed44c2269
Frank Fejes <frank@fejes.net>
2011-02-01 12:00:02 -0600

    nc

--- hbase-test.pl
+++ hbase-test.pl
@@ -7,4 +7,9 @@ my $host = $ARGV[0] || 'http://trpchads01.vm.searshc.com:8080';
 
 my $h = HBase->new(server => $host, debug => 1);
 
+my @tables = $h->list;
+foreach my $t (@tables) {
+    say "table: $t";
+}
+
 print "after module\n";
--- lib/HBase.pm
+++ lib/HBase.pm
@@ -19,20 +19,19 @@ our $VERSION = '0.0001';
 sub new {
     my $class   = shift;
     my $self    = {@_};
-    my $headers = {Accept => 'application/json'};
     my $client  = REST::Client->new();
 
     $self->{server} ||= '127.0.0.1:8080';
+    $self->{headers} = {Accept => 'application/json'};
 
     $client->setHost($self->{server});
-    $client->GET('/version', $headers);
+    $client->GET('/version', $self->{headers});
     if ($client->responseCode() ne '200') {
         warn "error: cannot connect to HBase at $self->{server}";
         return undef;
     }
 
     my $response = decode_json($client->responseContent());
-    print Dumper($response) if $self->{debug};
     if ($response->{REST}) {
         $self->{rest_version} = $response->{REST};
     }
@@ -44,6 +43,34 @@ sub new {
 
     bless($self, $class);
     $self;
+}
+
+sub good_response {
+    my $self = shift;
+    my $client = $self->{client};
+    my $code = $client->responseCode();
+
+    if ((! $code) || ($code ne '200')) {
+        warn "error: received response code ", $client->code;
+        return 0;
+    }
+
+    $code;
+}
+
+sub list {
+    my $self = shift;
+    my $client = $self->{client};
+
+    $client->GET('/', $self->{headers});
+    return undef if not $self->good_response;
+
+    my $tables_ref = decode_json($client->responseContent());
+    map {$_->{name}} @{$tables_ref->{table}};
+}
+
+sub get {
+    my $self = shift;
 }
 
 1;
